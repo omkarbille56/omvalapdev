3e43fee26c8b02f2d344be26e15655e4e5d96fa9
omkarbille56 <160456527+omkarbille56@users.noreply.github.com>
2026-03-06 00:22:28 +0530

    Create rules.py

---
+++ rules.py
@@ -0,0 +1,28 @@
+{
+  "rules": {
+    "cars": {
+      ".read": true,
+      ".write": true
+    },
+    "history": {
+      ".read": "auth != null",
+      ".write": "auth != null"
+    },
+    "activity": {
+      ".read": "auth != null",
+      ".write": "auth != null"
+    },
+    "admins": {
+      ".read": "auth != null",
+      ".write": "auth != null"
+    },
+    "valets": {
+      ".read": "auth != null",
+      ".write": "auth != null"
+    },
+    "meta": {
+      ".read": "auth != null",
+      ".write": "auth != null"
+    }
+  }
+}
